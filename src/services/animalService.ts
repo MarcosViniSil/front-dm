95747058ca3d7fa8c51c1f09e85fbc847cc17d59
MarcosViniSil <marcossilv203@gmail.com>
2026-03-22 22:29:22 -0300

    feat: create page to show animals location

--- src/services/animalService.ts
+++ src/services/animalService.ts
@@ -8,9 +8,28 @@ export interface Animal {
   weight: string;
 }
 
+export interface AnimalCharacteristics {
+  habitat: string;
+  region: string;
+  practice: string;
+  location: string;
+}
+
+export interface AnimalLocation {
+  id: number;
+  name: string;
+  imageUrl: string;
+  location: {
+    latitude: number;
+    longitude: number;
+  };
+  locationDescription: string;
+}
+
 export const animalService = {
-  getAnimals: (offset: number) =>
-    api.get<Animal[]>('/animals', {
-      params: { offset: String(offset) },
-    }),
+  getAnimals: (offset: number = 0) =>
+    api.get<Animal[]>('/animals', { params: { offset: offset.toString() } }),
+
+  getLocations: (offset: number = 0) =>
+    api.get<AnimalLocation[]>('/animals/location', { params: { offset: offset.toString() } }),
 };
