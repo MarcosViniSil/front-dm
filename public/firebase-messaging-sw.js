370336f8f7686dbd90c4ee3cec92014b3f6e64d1
MarcosViniSil <marcossilv203@gmail.com>
2026-04-04 13:41:28 -0300

    fix: adjust environment variables

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,26 +1,22 @@
 importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js');
 
-
-// Initialize the Firebase app in the service worker by passing the generated config
+// Usando as variáveis de ambiente do Vite
 const firebaseConfig = {
-    apiKey: "AIzaSyC8drfAGMjO_NZyp81ZmVpCaKIPZZnYkE4",
-    authDomain: "notificationpwa-96fa8.firebaseapp.com",
-    projectId: "notificationpwa-96fa8",
-    storageBucket: "notificationpwa-96fa8.firebasestorage.app",
-    messagingSenderId: "252969259945",
-    appId: "1:252969259945:web:e1f2d4693de7c47e8e2ab8",
-    measurementId: "G-05Q9HRR5ZC"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
-
 
 firebase.initializeApp(firebaseConfig);
 
 // Retrieve firebase messaging
 const messaging = firebase.messaging();
 
-// Handle incoming messages while the app is not in focus (i.e in the background, hidden behind other tabs, or completely closed).
 messaging.onBackgroundMessage(function(payload) {
   console.log('Received background message ', payload);
-
 });
