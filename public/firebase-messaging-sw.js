d3ddda5cf68577f39fadc5043117b35de0bbc79c
MarcosViniSil <marcossilv203@gmail.com>
2026-04-04 13:55:11 -0300

    chore: just for test

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -3,13 +3,13 @@ importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js');
 
 // Usando as variáveis de ambiente do Vite
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyC8drfAGMjO_NZyp81ZmVpCaKIPZZnYkE",
+  authDomain: "notificationpwa-96fa8.firebaseapp.com",
+  projectId: "notificationpwa-96fa8",
+  storageBucket: "notificationpwa-96fa8.firebasestorage.app",
+  messagingSenderId: "252969259945",
+  appId: "G-05Q9HRR5ZC",
+  measurementId: "BC8fe8Ei3tv7ITyhf9bW3CvZFyK3Udbh5_1OHIKwyfBETiu2fz8geBmpSlLPGtXVhnp9YJMaJakPovk6Qi-1baI"
 };
 
 firebase.initializeApp(firebaseConfig);
